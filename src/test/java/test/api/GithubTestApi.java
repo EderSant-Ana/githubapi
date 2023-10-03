d1fcb75457db138bf374577481132389434c3341
EderSant-Ana <edersana@gmail.com>
2023-10-03 19:14:52 -0300

    Env file included

--- src/test/java/test/api/GithubTestApi.java
+++ src/test/java/test/api/GithubTestApi.java
@@ -1,24 +1,32 @@
 package test.api;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.*;
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 
 public class GithubTestApi {
-	
-  @Test
-  public void shouldReturnTheListOfRepositoriesInGitHubOrganization() {
-	  
-      //Eder-Sant-ana
-	  //bearer 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
-	  
-      given().headers("Accept", "application/vnd.github+json", 
-    		  "Authorization", "Bearer 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",
-    		  "X-GitHub-Api-Version", "2022-11-28")
-      
-      .when().get("https://api.github.com/orgs/Eder-Sant-ana/repos")
-      .then()
-      .log().all()
-      .statusCode(200);
-      
-  }
+
+	public static final String URL = "https://api.github.com/orgs/";
+	public static final Dotenv dotenv = Dotenv.load();
+	public static final String TOKEN = dotenv.get("BEARERTOKEN");
+	public static final String ORGANIZATION = dotenv.get("ORGANIZATION");
+
+	@Test
+	public void shouldReturnTheListOfRepositoriesInGitHubOrganization() {
+
+		given().headers("Accept", "application/vnd.github+json", "Authorization",
+				String.format("Bearer %s", TOKEN), 
+				"X-GitHub-Api-Version", "2022-11-28")
+		.when()
+				.get(URL + ORGANIZATION + "/repos")
+		.then()
+			.log()
+			.all()
+			.statusCode(200)
+			.body("size()", greaterThan(0));
+
+	}
 }
